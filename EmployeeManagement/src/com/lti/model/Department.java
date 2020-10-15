f2de8bea3fc01325d635ddfd55b37375b33bb5ba
Guna <Guna@DESKTOP-TDELNE0>
2020-10-15 13:18:19 +0530

    added department class

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,23 @@
+package com.lti.model;
+
+public class Department {
+	int depid;
+	String depName;
+	public int getDepid() {
+		return depid;
+	}
+	public void setDepid(int depid) {
+		this.depid = depid;
+	}
+	public String getDepName() {
+		return depName;
+	}
+	public void setDepName(String depName) {
+		this.depName = depName;
+	}
+	
+	
+	
+	
+
+}
